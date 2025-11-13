b8c75791203f163a6dc468b24f2236085591104e
ColbyPrescott <colbyrprescott@gmail.com>
2025-11-13 11:36:47 -0500

    Update layout for landscape and portrait
    Move the sidebar to the left for desktop and to the bottom for mobile.
    This should give enough space to the sidebar and simulation no matter the device used.

--- main.js
+++ main.js
@@ -40,8 +40,21 @@ render.canvas.addEventListener("mousedown", (event) => {
     Composite.add(engine.world, rectangle);
 });
 
-function onWindowResize() {
+let mobile = false;
+function onWindowResizeDesktop() {
+    document.querySelectorAll(".mobile").forEach(elem => elem.classList.replace("mobile", "desktop"));
     Render.setSize(render, window.innerWidth * 0.7, window.innerHeight);
+}
+
+function onWindowResizeMobile() {
+    document.querySelectorAll(".desktop").forEach(elem => elem.classList.replace("desktop", "mobile"));
+    Render.setSize(render, window.innerWidth, window.innerHeight * 0.6);
+}
+
+function onWindowResize() {
+    mobile = window.innerWidth < window.innerHeight;
+    if(mobile) onWindowResizeMobile();
+    else onWindowResizeDesktop();
 }
 window.addEventListener("resize", onWindowResize);
 onWindowResize();
