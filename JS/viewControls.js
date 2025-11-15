2aff99ab60696c9466a4a48f92a400ea7bfc0bb4
ColbyPrescott <colbyrprescott@gmail.com>
2025-11-14 22:02:20 -0500

    Add mouse wheel zoom controls
    Scrolling the mouse wheel up and down will zoom in and out of the point where the cursor is located.

--- JS/viewControls.js
+++ JS/viewControls.js
@@ -4,17 +4,23 @@ render.canvas.addEventListener("wheel", (e) => {
     let scaleFactor = 0.9;
     if(e.wheelDeltaY < 0) scaleFactor = 1 / scaleFactor;
 
+    // Get current values
     let scaleX = render.bounds.max.x - render.bounds.min.x;
     let scaleY = render.bounds.max.y - render.bounds.min.y;
-    let centerX = (render.bounds.max.x + render.bounds.min.x) / 2;
-    let centerY = (render.bounds.max.y + render.bounds.min.y) / 2;
     let mouseX = map(e.offsetX, 0, render.canvas.width, render.bounds.min.x, render.bounds.max.x);
     let mouseY = map(e.offsetY, 0, render.canvas.height, render.bounds.min.y, render.bounds.max.y);
+    let centerX = (render.bounds.min.x + render.bounds.max.x) / 2;
+    let centerY = (render.bounds.min.y + render.bounds.max.y) / 2;
 
+    // Transform
     scaleX *= scaleFactor;
     scaleY *= scaleFactor;
-    render.bounds.min.x = mouseX - scaleX / 2;
-    render.bounds.min.y = mouseY - scaleY / 2;
-    render.bounds.max.x = mouseX + scaleX / 2;
-    render.bounds.max.y = mouseY + scaleY / 2;
+    centerX = lerp(centerX, mouseX, 1 - scaleFactor);
+    centerY = lerp(centerY, mouseY, 1 - scaleFactor);
+
+    // Apply
+    render.bounds.min.x = centerX - scaleX / 2;
+    render.bounds.min.y = centerY - scaleY / 2;
+    render.bounds.max.x = centerX + scaleX / 2;
+    render.bounds.max.y = centerY + scaleY / 2;
 });
