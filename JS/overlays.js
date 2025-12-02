69d456ecfbfc32568572ea23e28e15bfa8b995fb
ColbyPrescott <colbyrprescott@gmail.com>
2025-12-02 15:02:22 -0500

    Add more detailed drag overlay
    Show a circle when adding a circle and show a rectangle for everything else.
    The line was okay for the circle, perhaps a little difficult to predict the opposite side, but it looked very weird to draw the rectangle's diagonal.

--- JS/overlays.js
+++ JS/overlays.js
@@ -5,13 +5,28 @@ function drawDragOverlay() {
     render.context.save();
     render.context.resetTransform();
 
-    render.context.beginPath();
-    render.context.moveTo(Input.mouse.startPosition.canvasX, Input.mouse.startPosition.canvasY);
-    render.context.lineTo(Input.mouse.currentPosition.canvasX, Input.mouse.currentPosition.canvasY);
-
     render.context.strokeStyle = "#bbbbbb";
     render.context.lineWidth = 2;
-    render.context.stroke();
+
+    if(sidebar.shapeDropdown.value == "circle") {
+        render.context.beginPath();
+        render.context.arc(
+            Input.mouse.startPosition.canvasX,
+            Input.mouse.startPosition.canvasY,
+            Math.sqrt(Input.mouse.pageDeltaX * Input.mouse.pageDeltaX + Input.mouse.pageDeltaY * Input.mouse.pageDeltaY),
+            0, Math.PI * 2
+        );
+        render.context.moveTo(Input.mouse.startPosition.canvasX, Input.mouse.startPosition.canvasY);
+        render.context.lineTo(Input.mouse.currentPosition.canvasX, Input.mouse.currentPosition.canvasY);
+        render.context.stroke();
+    } else {
+        render.context.strokeRect(
+            Input.mouse.startPosition.canvasX,
+            Input.mouse.startPosition.canvasY,
+            Input.mouse.currentPosition.canvasX - Input.mouse.startPosition.canvasX,
+            Input.mouse.currentPosition.canvasY - Input.mouse.startPosition.canvasY
+        );
+    }
 
     render.context.restore();
 }
