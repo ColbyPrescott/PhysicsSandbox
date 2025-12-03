323e4bb6274f31ae764f2b1a71d4728ef8fb7f50
ColbyPrescott <colbyrprescott@gmail.com>
2025-12-03 17:03:50 -0500

    Finish remove click
    It is now possible to click on single objects to remove them from the simulation.
    After spawning a bunch of objects, I might want to go back.

--- JS/actionRemove.js
+++ JS/actionRemove.js
@@ -2,8 +2,18 @@ render.canvas.addEventListener("mousedown", e => {
     Input.mouse.startPosition.setPositionFromEvent(e);
     Input.mouse.currentPosition.setPositionFromEvent(e);
 
-    if(sidebar.actionDropdown != "remove") return;
-    if(sidebar.brushDropdown != "click") return;
+    if(sidebar.actionDropdown.value != "remove") return;
+    if(sidebar.brushDropdown.value != "click") return;
 
-    
+    const mousePosition = {
+        x: Input.mouse.currentPosition.worldX,
+        y: Input.mouse.currentPosition.worldY
+    };
+
+    let targetBody;
+    for(let body of Composite.allBodies(engine.world).reverse())
+        if(bodyPointIntersection(body, mousePosition)) targetBody = body;
+    if(!targetBody) return;
+
+    Composite.remove(engine.world, targetBody);
 });
--- JS/engine.js
+++ JS/engine.js
@@ -1,10 +1,13 @@
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Bounds = Matter.Bounds;
+const Composite = Matter.Composite;
+const Detector = Matter.Detector;
 const Engine = Matter.Engine;
+const Events = Matter.Events;
 const Render = Matter.Render;
 const Runner = Matter.Runner;
-const Body = Matter.Body;
-const Bodies = Matter.Bodies;
-const Composite = Matter.Composite;
-const Events = Matter.Events;
+const Vertices = Matter.Vertices;
 
 const engine = Engine.create();
 
@@ -18,3 +21,22 @@ const render = Render.create({
 });
 
 const runner = Runner.create();
+
+function bodyPointIntersection(body, point, pointCollisionFilter) {
+    pointCollisionFilter ??= {
+        category: 0x0001,
+        mask: 0xFFFFFFFF,
+        group: 0
+    };
+    
+    if(!Bounds.contains(body.bounds, point)) return false;
+    if(!Detector.canCollide(body.collisionFilter, pointCollisionFilter)) return false;
+
+    for (let i = body.parts.length > 1 ? 1 : 0; i < body.parts.length; i++) {
+        const part = body.parts[i];
+        if (Vertices.contains(part.vertices, point))
+            return true;
+    }
+
+    return false;
+}
